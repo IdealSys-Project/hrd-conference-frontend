6a156f4d259e301bfaa1990705b67c9fba031f0f
zakiiizakaria <zakihelmi1997@gmail.com>
2025-04-26 00:25:30 +0800

    implement target audience section UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import EventCountdown from '@/components/sections/event-countdown/event-countdow
 import EventOverview from '@/components/sections/event-overview/event-overview';
 import KeyMainTheme from '@/components/sections/key-main-theme/key-main-theme';
 import EventBenefits from '@/components/sections/event-benefits/event-benefits';
+import TargetAudience from '@/components/sections/target-audience/target-audience';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <EventOverview />
       <KeyMainTheme />
       <EventBenefits />
+      <TargetAudience />
     </main>
   );
 }
