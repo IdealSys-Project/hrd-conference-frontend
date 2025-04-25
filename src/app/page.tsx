9d6f284b13c03f431e8f3db5e831a02069f44b65
zakiiizakaria <zakihelmi1997@gmail.com>
2025-04-25 16:38:38 +0800

    implement key-main-theme section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 
 import EventCountdown from '@/components/sections/event-countdown/event-countdown';
 import EventOverview from '@/components/sections/event-overview/event-overview';
+import KeyMainTheme from '@/components/sections/key-main-theme/key-main-theme';
 
 export default function Home() {
   return (
     <main>
       <EventCountdown />
       <EventOverview />
+      <KeyMainTheme />
     </main>
   );
 }
