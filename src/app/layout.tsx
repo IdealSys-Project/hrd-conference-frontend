de8ffee8fd1829c26ab923fd080d8a3e44b981cb
zakiiizakaria <zakihelmi1997@gmail.com>
2025-04-26 21:27:23 +0800

    Implement sidebar social media UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Poppins } from "next/font/google";
 import Providers from "@/app/provider";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer/Footer";
+import Sidebar from "@/components/layout/Sidebar/Sidebar";
 import { ReactNode } from "react";
 
 const poppins = Poppins({
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body className={`${poppins.className} antialiased`}>
         <Providers>
           <Header />
+          <Sidebar />
           {children}
           <Footer />
         </Providers>
